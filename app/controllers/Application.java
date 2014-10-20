3f96e4da0fb68012915c9ee4bfe58d32a6e516d6
Bradley Davis <bradley.davis@unc.edu>
2014-10-20 15:57:11 -0400

    Add mechanism to take survey and collect results

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,17 @@
 package controllers;
 
-import models.User;
-import play.*;
+import models.OutstandingSurveyResult;
+
+import play.libs.ws.*;
+import play.mvc.Result;
+
+import static play.libs.F.Function;
+import static play.libs.F.Promise;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.UUID;
 
 public class Application extends Controller {
 
@@ -12,23 +19,39 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
-    public static Result redirect() {
-        return redirect("http://www.google.com");
+    public static Result takeDemoSurvey() {
+
+        OutstandingSurveyResult s = new OutstandingSurveyResult();
+        s.setMedicalId("720326365");
+        s.setSurveyId("SV_0JSC2ShChssXa61");
+        s.setRequestId(UUID.randomUUID().toString());
+        s.insert();
+
+        String url = "https://unc.az1.qualtrics.com/SE/?";
+        url += "SID=" + s.getSurveyId();
+        url += "&requestId=" + s.getRequestId();
+
+        return redirect(url);
     }
 
-    public static Result add() {
+    public static Promise<Result> completeDemoSurvey(String requestId, String responseSetId) {
 
-        User u = new User();
-        u.name = "Bradley";
-        u.insert();
-        return ok("Inserted");
+        OutstandingSurveyResult s = OutstandingSurveyResult.findByRequestId(requestId);
+        s.setResponseSetId(responseSetId);
 
-    }
+        String url = "https://survey.qualtrics.com/WRAPI/ControlPanel/api.php?Request=getLegacyResponseData&Format=JSON&User=720326365%23unc&Token=czqtiOXIJOk7nbZ0J8wlfiYn2wNjhKxntyhRChXi&Version=2.4";
+        url += "&SurveyID=" + s.getSurveyId();
+        url += "&ResponseID=" + s.getResponseSetId();
 
-    public static Result show() {
+        final Promise<Result> resultPromise = WS.url(url).get().map(
+                new Function<WSResponse, Result>() {
+                    public Result apply(WSResponse response) {
+                        return ok("Response:" + response.asJson());
+                    }
+                }
+        );
 
-        User u = User.findByName("Bradley");
-        return ok(u.name);
+        return resultPromise;
 
     }
 
